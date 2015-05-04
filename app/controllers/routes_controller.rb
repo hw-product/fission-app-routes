7c7b95cf4d7b9dfdd11333aaa32c9665137c1826
Chris Roberts <code@chrisroberts.org>
2015-05-04 07:22:23 -0700

    Include support for custom services within composed routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -21,11 +21,48 @@ class RoutesController < ApplicationController
       format.html do
         @services = @account.product_features.map(&:services).flatten.uniq.sort_by(&:name)
         @service_groups = @account.product_features.map(&:service_groups).flatten.uniq.sort_by(&:name)
+        @custom_services = @account.custom_services_dataset.order(:name).all
       end
     end
   end
 
   def create
+    respond_to do |format|
+      format.js do
+        flash[:error] = 'Unsupported request!'
+        javascript_redirect_to dashboard_path
+      end
+      format.html do
+        route = Route.create(
+          :name => params[:name],
+          :description => params[:description],
+          :account_id => @account.id
+        )
+        @services = @account.product_features.map(&:services).flatten.uniq.sort_by(&:name)
+        @service_groups = @account.product_features.map(&:service_groups).flatten.uniq.sort_by(&:name)
+        @custom_services = @account.custom_services
+        params.fetch(:service, []).each do |position, srv_id|
+          route.add_service(
+            :position => position,
+            :service => @services.detect{|s| s.id == srv_id.to_i}
+          )
+        end
+        params.fetch('service-group', []).each do |position, grp_id|
+          route.add_service_group(
+            :position => position,
+            :service_group => @service_groups.detect{|s| s.id == grp_id.to_i}
+          )
+        end
+        params.fetch('custom-service', []).each do |position, grp_id|
+          route.add_custom_service(
+            :position => position,
+            :custom_service => @custom_services.detect{|s| s.id == grp_id.to_i}
+          )
+        end
+        flash[:success] = 'Created new route!'
+        redirect_to routes_path
+      end
+    end
   end
 
   def edit
@@ -36,6 +73,9 @@ class RoutesController < ApplicationController
       end
       format.html do
         @route = @account.routes_dataset.where(:id => params[:id]).first
+        @services = @account.product_features.map(&:services).flatten.uniq.sort_by(&:name) - (@route.services || [])
+        @service_groups = @account.product_features.map(&:service_groups).flatten.uniq.sort_by(&:name) - (@route.service_groups || [])
+        @custom_services = @account.custom_services_dataset.order(:name).all - (@route.custom_services || [])
         unless(@route)
           flash[:error] = 'Failed to locate requested route'
           redirect_to routes_path
@@ -45,6 +85,50 @@ class RoutesController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      format.js do
+        flash[:error] = 'Unsupported request!'
+        javascript_redirect_to dashboard_path
+      end
+      format.html do
+        route = Route.find_by_id(params[:id])
+        if(route)
+          route.remove_all_services
+          route.remove_all_custom_services
+          route.remove_all_service_groups
+          unless(route.description == params[:description])
+            route.description = params[:description]
+            route.save
+          end
+          @services = @account.product_features.map(&:services).flatten.uniq.sort_by(&:name)
+          @service_groups = @account.product_features.map(&:service_groups).flatten.uniq.sort_by(&:name)
+          @custom_services = @account.custom_services
+          params.fetch(:service, []).each do |position, srv_id|
+            route.add_service(
+              :position => position,
+              :service => @services.detect{|s| s.id == srv_id.to_i}
+            )
+          end
+          params.fetch('service-group', []).each do |position, grp_id|
+            route.add_service_group(
+              :position => position,
+              :service_group => @service_groups.detect{|s| s.id == grp_id.to_i}
+            )
+          end
+          params.fetch('custom-service', []).each do |position, grp_id|
+            route.add_custom_service(
+              :position => position,
+              :custom_service => @custom_services.detect{|s| s.id == grp_id.to_i}
+            )
+          end
+          flash[:success] = 'Updated route!'
+          redirect_to routes_path
+        else
+          flash[:error] = 'Failed to locate requested route!'
+          redirect_to routes_path
+        end
+      end
+    end
   end
 
   def destroy
