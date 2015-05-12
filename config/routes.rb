26135adc4d7b178a506f6282a600056d82e5ca9d
Chris Roberts <code@chrisroberts.org>
2015-05-12 13:58:16 -0700

    New routes. Allow optional pipeline name where desired.

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,17 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'pipeline/:pipeline_name/dashboard', :as => 'pipeline_dashboard', :to => 'routes#dashboard'
+
   # Repository hooking
-  get 'pipeline/repositories', :as => 'pipeline_repositories', :to => 'route_repositories#list'
+  get 'pipeline(/:pipeline_name)/repositories', :as => 'pipeline_repositories', :to => 'route_repositories#list'
   post 'pipeline/repositories/:repository_id', :as => 'pipeline_repository_enable', :to => 'route_repositories#enable'
   delete 'pipeline/repositories/:repository_id', :as => 'pipeline_repository_disable', :to => 'route_repositories#disable'
   get 'pipeline/repositories/validate/:repository_id', :as => 'pipeline_repository_validate', :to => 'route_repositories#validate'
   get 'pipeline/repositories/reload', :as => 'pipeline_repositories_reload', :to => 'route_repositories#reload'
 
   # Job display
-  get 'pipeline/jobs(/:payload_filter/:payload_value)', :to => 'route_jobs#all', :as => 'pipeline_jobs'
+  get 'pipeline(/:pipeline_name)/jobs(/:payload_filter/:payload_value)', :to => 'route_jobs#all', :as => 'pipeline_jobs'
   get 'pipeline/job/:job_id', :to => 'route_jobs#details', :as => 'pipeline_job'
 
 end
