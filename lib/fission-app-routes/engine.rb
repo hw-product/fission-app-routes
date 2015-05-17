5ef489b784ac0560342751c0cbcc342eb4d7cb1e
Chris Roberts <code@chrisroberts.org>
2015-05-17 12:26:32 -0700

    Auto create custom route on order. Force redirects based on isolation mode.

--- lib/fission-app-routes/engine.rb
+++ lib/fission-app-routes/engine.rb
@@ -5,13 +5,13 @@ module FissionApp
 
       def route_set_navigation
         nav, acct_nav = non_route_set_navigation
-        if(isolated_product? && !session[:route_id])
-          session[:route_id] = @account.routes_dataset.where(:name => @product.internal_name).first.try(:id)
+        if(isolated_product? && session[:route_id].to_i == 0)
+          session[:route_id] = @account.routes_dataset.where(:name => @product.internal_name).first.try(:id) || 0
         end
         if(session[:route_id])
           @pipeline = @account.routes_dataset.where(:id => session[:route_id]).first
         end
-        if(@account.products.include?(Fission::Data::Models::Product.find_by_internal_name('routes')))
+        if(current_user.run_state.products.include?(Fission::Data::Models::Product.find_by_internal_name('routes')))
           pipe_list = Smash.new.tap do |pipes|
             @account.routes.each do |route|
               pipes[route.name.humanize] = Rails.application.routes.url_helpers.route_path(route.id)
@@ -19,14 +19,22 @@ module FissionApp
             pipes['break'] = nil
             pipes['New'] = Rails.application.routes.url_helpers.new_route_path
           end
-          if(@pipeline)
-            @navigation = Smash.new(
-              @pipeline.name.humanize => pipe_list,
-              'Dashboard' => pipeline_dashboard_path(:pipeline_name => @pipeline.name),
-              'Manage' => edit_route_path(@pipeline.id),
-              'Repositories' => pipeline_repositories_path(:pipeline_name => @pipeline.name),
-              'Jobs' => pipeline_jobs_path(:pipeline_name => @pipeline.name)
-            )
+          if(@pipeline || isolated_product?)
+            if(@pipeline)
+              @navigation = Smash.new.tap do |_nav|
+                unless(isolated_product?)
+                  _nav[@pipeline.name.humanize] = pipe_list
+                end
+                _nav['Dashboard'] = pipeline_dashboard_path(:pipeline_name => @pipeline.name)
+                if(current_user.run_state.product_features.map(&:name).include?('routes_full_access'))
+                  _nav['Manage'] = edit_route_path(@pipeline.id)
+                end
+                _nav['Repositories'] = pipeline_repositories_path(:pipeline_name => @pipeline.name)
+                _nav['Jobs'] = pipeline_jobs_path(:pipeline_name => @pipeline.name)
+              end
+            else
+              @navigation = Smash.new
+            end
           else
             @navigation = Smash.new(
               'Pipelines' => pipe_list
@@ -52,12 +60,52 @@ module FissionApp
         # Hooks!
 
         c_b = Rails.application.config.settings.fetch(:callbacks, :before, :dashboard, :summary, Smash.new)
+        # Root dashboard removes selected route when not in isolation mode
         c_b[:scrub_route] = lambda do|*_|
-          if(session.delete(:route_id))
+          if(!isolated_product? && session.delete(:route_id))
             @pipeline = nil
           end
         end
+        # Auto-redirect to route creation when not in isolation mode
+        # and account does not have any routes defined
+        c_b[:new_route_when_no_routes] = lambda do |*_|
+          if(!isolated_product? && !current_user.run_state.plans.empty? && @account.routes_dataset.count == 0)
+            redirect_to new_route_path
+            return false
+          end
+        end
+        c_b[:send_to_product_dashboard] = lambda do |*_|
+          if(isolated_product? && session[:route_id].to_i == 0)
+            session[:route_id] = @account.routes_dataset.where(:name => @product.internal_name).first.try(:id) || 0
+          end
+          pipeline = @account.routes_dataset.where(:id => session[:route_id]).first
+          if(pipeline)
+            redirect_to pipeline_dashboard_path(:pipeline_name => pipeline.name)
+          end
+        end
         Rails.application.config.settings.set(:callbacks, :before, :dashboard, :summary, c_b)
+
+        c_b = Rails.application.config.settings.fetch(:callbacks, :after, 'account/billing', :order, Smash.new)
+        # Auto create custom route when plan is attached to service
+        # group'ed product
+        c_b[:plan_route_autocreation] = lambda do |*_|
+          if(isolated_product? && @plan && @plan.product && @plan.product.service_group)
+            unless(@account.routes_dataset.where(:name => @plan.product.internal_name).count > 0)
+              route = Fission::Data::Models::Route.create(
+                :name => @plan.product.internal_name,
+                :account_id => @account.id,
+                :description => @plan.product.service_group.description
+              )
+              @plan.product.service_group.services.each_with_index do |srv, idx|
+                route.add_service(
+                  :service => srv,
+                  :position => idx
+                )
+              end
+            end
+          end
+        end
+        c_b = Rails.application.config.settings.set(:callbacks, :after, 'account/billing', :order, c_b)
 
         product = Fission::Data::Models::Product.find_by_internal_name('routes')
         unless(product)
