d83d06c45b4b0be997bf3a578a92e3a327dc09cb
Chris Roberts <code@chrisroberts.org>
2015-05-30 07:40:54 -0700

    Only redirect if user has access to destination

--- lib/fission-app-routes/engine.rb
+++ lib/fission-app-routes/engine.rb
@@ -68,7 +68,9 @@ module FissionApp
         # and account does not have any routes defined
         c_b[:new_route_when_no_routes] = lambda do |*_|
           if(!isolated_product? && !current_user.run_state.plans.empty? && @account.routes_dataset.count == 0)
-            redirect_to prebuilt_routes_path
+            if(current_user.valid_path?(prebuilt_routes_path))
+              redirect_to prebuilt_routes_path
+            end
           end
         end
         c_b[:send_to_product_dashboard] = lambda do |*_|
